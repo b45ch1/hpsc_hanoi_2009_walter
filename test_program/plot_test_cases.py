4b1d9740d927bdc28adccc67dc0a83deaa9c5897
Sebastian Walter <sebastian.walter@gmail.com>
2010-09-23 10:44:11 +0200

    added utp test cases

--- test_program/plot_test_cases.py
+++ test_program/plot_test_cases.py
@@ -1,7 +1,8 @@
-
 import os
 import ctypes
 import numpy
+import matplotlib.pyplot as pyplot
+import prettyplotting
 
 lib = numpy.ctypeslib.load_library('libtest_cases', os.path.dirname(__file__))
 
@@ -10,21 +11,81 @@ c_int_ptr    = ctypes.POINTER(ctypes.c_int)
 c_int        = ctypes.c_int
 c_double     = ctypes.c_double
 
-argtypes1 = [ctypes.c_int, c_double_ptr]
+argtypes1 = [c_int, c_double_ptr]
+argtypes2 = [c_int, c_int, c_int, c_double_ptr]
+
 lib.function_inv_c.argtypes = argtypes1
 lib.function_inv_adolc.argtypes = argtypes1
 lib.gradient_trace_adolc.argtypes = argtypes1
+lib.gradient_trace_c.argtypes = argtypes1
+
+lib.utp_dot_adolc.argtypes = argtypes2
+lib.utp_dot_taylorpoly.argtypes = argtypes2
+lib.utps_dot_taylorpoly.argtypes = argtypes2
+
+
+
 
 
 runtimes = numpy.zeros(2, dtype=float)
-for n in range(50,100,10):
-    runtimes[:] = 0
-    lib.function_inv_c(n, runtimes.ctypes.data_as(c_double_ptr))
-    print runtimes
-    runtimes[:] = 0
-    lib.function_inv_adolc(n, runtimes.ctypes.data_as(c_double_ptr))
-    print runtimes
+
+# # function evaluation
+# for n in range(50,100,10):
+#     print 'N = ',n
+#     runtimes[:] = 0
+#     lib.function_inv_c(n, runtimes.ctypes.data_as(c_double_ptr))
+#     print runtimes
+#     runtimes[:] = 0
+#     lib.function_inv_adolc(n, runtimes.ctypes.data_as(c_double_ptr))
+#     print runtimes
+
+# gradient evaluation
+# for n in range(50,130,10):
+#     print 'N = ',n
+#     runtimes[:] = 0
+#     lib.gradient_trace_adolc(n, runtimes.ctypes.data_as(c_double_ptr))
+#     print runtimes
     
-    runtimes[:] = 0
-    lib.gradient_trace_adolc(n, runtimes.ctypes.data_as(c_double_ptr))
-    print runtimes
+#     runtimes[:] = 0
+#     lib.gradient_trace_c(n, runtimes.ctypes.data_as(c_double_ptr))
+#     print runtimes
+
+
+# utp arithmetic
+
+num_methods = 4
+N_list = [50,80,100,120]
+D_list = [10]
+P_list = [1]
+
+runtimes_matrix = numpy.zeros((num_methods, len(P_list), len(D_list), len(N_list)), dtype=float)
+
+for p,P in enumerate(P_list):
+    for d,D in enumerate(D_list):
+        for n,N in enumerate(N_list):
+            print 'P,D,N = ',P,D,N
+            runtimes[:] = 0
+            lib.utp_dot_adolc(P, D, N, runtimes.ctypes.data_as(c_double_ptr))
+            runtimes_matrix[0,p,d,n] = runtimes[0]
+            runtimes_matrix[1,p,d,n] = runtimes[1]
+            runtimes[:] = 0
+            lib.utp_dot_taylorpoly(P, D, N, runtimes.ctypes.data_as(c_double_ptr))
+            runtimes_matrix[2,p,d,n] = runtimes[0]
+            runtimes[:] = 0
+            lib.utps_dot_taylorpoly(P, D, N, runtimes.ctypes.data_as(c_double_ptr))
+            runtimes_matrix[3,p,d,n] = runtimes[0]
+
+print runtimes_matrix
+pyplot.figure()
+pyplot.semilogy(N_list, runtimes_matrix[0,0,0,:], '-.kv', label='ADOL-C taping')
+pyplot.semilogy(N_list, runtimes_matrix[1,0,0,:], '--k^', label='ADOL-C eval')
+pyplot.semilogy(N_list, runtimes_matrix[2,0,0,:], '-k.', label='TAYLORPOLY UTPM eval')
+pyplot.semilogy(N_list, runtimes_matrix[3,0,0,:], '-kd', label='TAYLORPOLY UTPS eval')
+
+pyplot.xlabel('matrix size $N$')
+pyplot.ylabel('runtime $t$ [ms]')
+pyplot.title(r'UTP, matrix-matrix product, P,D=%d,%d'%(P_list[0],D_list[0]))
+pyplot.legend(loc='best')
+
+pyplot.show()
+
--- test_program/test_cases.cpp
+++ test_program/test_cases.cpp
@@ -11,6 +11,8 @@ extern "C"{
 #include "dense_inverse.h"
 #include "inv_b.h"
 #include "utpm.h"
+#include "utps.h"
+
 
 }
 
@@ -163,6 +165,7 @@ extern "C"
 int gradient_trace_adolc(int N, double *runtimes){
     /*
 	compute gradient of trace(inv(A)) with ADOL-C
+	this is equivalent to Univariate Taylor Polynomial Arithmetic of Scalars
 	*/
 	double start_time, end_time;
 	double *A = new double[N*N];
@@ -211,9 +214,153 @@ int gradient_trace_adolc(int N, double *runtimes){
 	delete[] R;
 	delete[] Id;
 	
-	
 	return -1;
 }
+
+extern "C"
+int gradient_trace_c(int N, double *runtimes){
+    /*
+    compute gradient of trace(inv(A)) with a C implementation
+    this is equivalent to Univariate Taylor Polynomial Arithmetic of Matrices
+      
+    
+    need to compute
+    d tr(A^-1) = tr( - (A^-1)^2 dA) and therefore
+    \nabla tr(A^-1) = - ((A^-1)^2)^T
+    */
+    
+    double start_time, end_time;
+    double *A = new double[N*N];
+    double *Ainv = new double[N*N];
+    double *R = new double[N*N];
+    double *Abar = new double[N*N];
+    
+    /* fill A with random numbers */
+    for(int n = 0; n!=N; ++n){
+        for(int m = 0; m!=N; ++m){
+            A[id(n,m,N)] = rand()/100000000000.;
+        //     Ainv[id(n,m,N)] =  A[id(n,m,N)];
+        }
+    }
+    start_time = mtime();
+    
+    /* compute A^{-1} */
+    inv(A, Ainv, R, N);
+     /* compute Abar = - (A^{-1} A^{-1})^T */
+    cblas_dgemm(CblasRowMajor, CblasTrans,
+                 CblasTrans, N, N,
+                N, -1., Ainv,
+                 N, Ainv, N,
+                 0., Abar, N);
+    end_time = mtime();
+    runtimes[0] = end_time-start_time;
+    
+    delete[] A;
+    delete[] Ainv;
+    delete[] R;
+    delete[] Abar;
+    
+    return -1;
+}
+
+
+extern "C"
+int utp_dot_adolc(int P, int D, int N, double *runtimes){
+    /* UTP computation of the matrix matrix product using ADOL-C in the forward mode */
+    double start_time, end_time;
+    adouble *aA = new adouble[(N*N)];
+    adouble *aB = new adouble[(N*N)];
+    adouble *aC = new adouble[(N*N)];
+    double *x = new double[2*N*N];
+    double *y = new double[N*N];
+    double ***V = myalloc3(2*N*N,P, D-1);
+    double ***W = myalloc3(N*N,P, D-1);
+
+    start_time = mtime();
+    trace_on(2);
+    for(int n = 0; n!=N; ++n){
+        for(int m = 0; m!=N; ++m){
+            aA[id(n,m,N)]<<= rand()/100000000000.;
+            aB[id(n,m,N)]<<= rand()/100000000000.;
+        }
+    }
+    dot(aA, aB, aC, N);
+    for(int n = 0; n!=N; ++n){
+        for(int m = 0; m!=N; ++m){
+            aC[id(n,m,N)]>>= *y;
+        }
+    }
+    trace_off();
+    end_time = mtime();
+    runtimes[0] = end_time-start_time;
+    
+    start_time = mtime();
+    hov_forward(2,N*N,2*N*N,D-1, P, x, V, y, W);
+    end_time = mtime();
+    runtimes[1] = end_time-start_time;
+    
+    delete[] x;
+    delete[] y;
+    delete[] aA;
+    delete[] aB;
+    delete[] aC;
+    
+    return -1;
+}
+
+extern "C"
+int utp_dot_taylorpoly(int P, int D, int N, double *runtimes){
+    /* UTP computation of the matrix matrix product using TAYLORPOLY in the forward mode */
+    
+    double start_time, end_time;
+    double *utpm_A = new double[(1+(D-1)*P)*(N*N)];
+    double *utpm_B = new double[(1+(D-1)*P)*(N*N)];
+    double *utpm_C = new double[(1+(D-1)*P)*(N*N)];
+    int *utpm_strides = new int[3];
+    utpm_strides[0] = N*N*sizeof(double);
+    utpm_strides[1] = sizeof(double);
+    utpm_strides[2] = N*sizeof(double);
+    int *utpm_shape = new int[2];
+    utpm_shape[0] = N; utpm_shape[1] = N;
+    
+    /* fill utpm_A and utpm_B with random numbers */
+    for(int i = 0; i!=N*N + (D-1)*P*(N*N); ++i){
+        utpm_A[i] = rand()/100000000000.;
+        utpm_B[i] = rand()/100000000000.;
+    }
+    
+    // print_utpm(P, D, 2, utpm_shape, &utpm_strides[1], utpm_A);
+    start_time = mtime();
+    utpm_dot(P, D, N, N, N, 1., utpm_A, utpm_strides, utpm_B, utpm_strides, 0., utpm_C, utpm_strides);
+    end_time = mtime();
+    runtimes[0] = end_time-start_time;
+    
+    return -1;
+}
+
+extern "C"
+int utps_dot_taylorpoly(int P, int D, int N, double *runtimes){
+    int stride = (1+P*(D-1));
+    double start_time, end_time;
+    double *x = new double[(1+(D-1)*P)*(N*N)];
+    double *y = new double[(1+(D-1)*P)*(N*N)];
+    double *z = new double[(1+(D-1)*P)*(N*N)];
+
+    start_time = mtime();
+    for(int n = 0; n != N; ++n){ // iterate over all columns
+       for(int m = 0; m != N; ++m){ // iterate over all rows
+            for(int k = 0; k != N; ++k){
+                utps_amul(P, D, x+fid(m,k,N)*stride, y+fid(k,n,N)*stride, z+fid(m,n,N)*stride);
+            }
+        }
+    }
+    end_time = mtime();
+    runtimes[0] = end_time-start_time;
+    return -1;
+    
+}
+    
+
 
 
  
